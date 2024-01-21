2d4b7510bacd7c7a192ef10a1669b95e02912091
Robakidze <robaqidzenikolozi14@gmail.com>
2024-01-22 00:03:18 +0400

    added cart :)

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
 const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
 
-  const [something, setSomething] = useState({
+  const [Mainpic, setMainpic] = useState({
     bigImage: assetsBaseUrl + "/" + product.images.originals[0]
   })
 
@@ -18,10 +18,18 @@ const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
     productAmount:0,
   })
 
-  
-
+  const [list,setlist] = useState(false);
+ 
   return (
     <>
+      <div className={!list ?'non-cart':'cart'}>
+        <div className='cart-up'>
+          <h4 className='cart-up-cart'>Cart</h4>
+        </div>
+        <div children className='cart-down'>
+          <h3 className='cart-down-comment'>Your cart is empty</h3>
+        </div>
+      </div>
       <div className='taskbar'>
         <div className='banner'><img className='logo' src="./project-files/assets/logo.svg" alt="" /></div>
         <div className='links'>
@@ -31,16 +39,24 @@ const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
           <a href="#">About</a>
           <a href="#">Contract</a>
         </div>
-        <button className='shopcart'><img className='shopcart-pic' src="./project-files/assets/icon-cart.svg" alt="" /></button>
+        <button className='shopcart' onClick={()=>{
+          if(!list){
+            setlist(true)
+          }else{
+            setlist(false)
+          }
+        }}>
+          <img className='shopcart-pic' src="./project-files/assets/icon-cart.svg" alt="" />
+        </button>
         <div className='profile-pic'><img className='picture' src={picture} alt="" /></div>
       </div>
       <div>
-        <img className='big-picture' src={something.bigImage} alt="" />
+        <img className='big-picture' src={Mainpic.bigImage} alt="" />
       </div>
       <div className='small-pics-div'>
         <div className='small-pics'>
           <button className='button-pic' onClick={()=>{
-            setSomething(
+            setMainpic(
                 {
                   bigImage:assetsBaseUrl + "/" + product.images.originals[0]
                 }
@@ -49,7 +65,7 @@ const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
             <img className='small-image' src={assetsBaseUrl + "/" + product.images.thumbnails[0]} alt="" ></img>
           </button>
           <button className='button-pic' onClick={()=>{
-            setSomething(
+            setMainpic(
                 {
                   bigImage:assetsBaseUrl + "/" + product.images.originals[1]
                 }
@@ -58,7 +74,7 @@ const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
             <img className='small-image' src={assetsBaseUrl + "/" + product.images.thumbnails[1]} alt="" ></img>
           </button>
           <button className='button-pic' onClick={()=>{
-            setSomething(
+            setMainpic(
                 {
                   bigImage:assetsBaseUrl + "/" + product.images.originals[2]
                 }
@@ -67,7 +83,7 @@ const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
             <img className='small-image' src={assetsBaseUrl + "/" + product.images.thumbnails[2]} alt="" ></img>
           </button>
           <button className='button-pic' onClick={()=>{
-            setSomething(
+            setMainpic(
                 {
                   bigImage:assetsBaseUrl + "/" + product.images.originals[3]
                 }
@@ -129,8 +145,13 @@ const picture = assetsBaseUrl + "/" + loggedInUser.profileImage;
             }}>+</button>
         </div>
       </div>
-      <div className='add-to-cart'>
-        <img src="../project-files/assets/icon-cart.svg" alt="" />
+      <div className='add-to-cart-div'>
+        <div className='add-to-cart'>
+          <div className='button-image' alt="">
+            <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/></svg>
+          </div>
+          <p className='button-text'>Add to cart</p>
+        </div>
       </div>
     </>
   )
